74f9b157a26dd905090be1b8881eb83dc16806d5
emillyvieira <emilly.vieira190505@gmail.com>
2024-02-22 21:20:45 -0300

    logger & etc

--- src/server.js
+++ src/server.js
@@ -2,15 +2,18 @@
 import express from 'express'
 import {PORT, HOST} from './config.js'
 import { users } from './db-memory/user.js'
+import logger from './middlewares/logger.js'
 // import qualquer from '.config.js'
 
 const app = express()
 
 app.use(express.json())
+app.use('/user', logger)
 
 app.get('/', (req, res) => {
     res.json({message: "Bem vindo a API!"})
 })
+
 
 app.get('/user', (req, res) => {
     res.json({
@@ -24,29 +27,40 @@ app.post('/user', (req, res) => {
     user.id = users[users.length - 1].id + 1
     users.push(user)
     res.json({
-        success: "Usuários listados com sucesso!",
+        success: "Usuários adicionado com sucesso!",
         users
+    })
+})
+
+
+app.delete('/user', (req, res) => {
+    const id = req.body.id
+    const usersResult = users.filter(user => user.id !== id)
+    res.json({
+        success: `Usuários ${id} removido com sucesso!`,
+        usersResult
     })
 })
 
 app.put('/user', (req, res) => {
-    const user = req.body
-    user.id = users[users.length - 1].id + 1
-    users.push(user)
-    res.json({
-        success: "Usuários listados com sucesso!",
-        users
+    const newUser = req.body
+    const usersResult = users.map(user => {
+        if(user.id === newUser.id) {
+            return {
+                id: user.id,
+                name: newUser.name || user.name,
+                email: newUser.email || user.email,
+                avatar:  newUser.avatar || user.avatar
+            }
+        }
+        return user
     })
-})
 
-app.delete('/user', (req, res) => {
-    const user = req.body
-    user.id = users[users.length - 1].id + 1
-    users.push(user)
     res.json({
-        success: "Usuários listados com sucesso!",
-        users
+        success: `Usuários ${newUser.id} atualizado com sucesso!`,
+        usersResult
     })
+    
 })
 
 app.listen(PORT, () => {
